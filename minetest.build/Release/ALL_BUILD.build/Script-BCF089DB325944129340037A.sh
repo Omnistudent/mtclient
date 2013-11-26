227e4dbd3cbc016ea15c0dbae0430f41124baa3a
Omnistudent <theostenman@yahoo.com>
2013-11-26 12:04:31 +0100

    16 removed cmake and build results files

--- minetest.build/Release/ALL_BUILD.build/Script-BCF089DB325944129340037A.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/security/mtclient -f /Users/security/mtclient/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
--- minetest.build/Release/install.build/Script-B4749FA6E73146F4BF6233CE.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/security/mtclient -f /Users/security/mtclient/CMakeScripts/install_postBuildPhase.make$CONFIGURATION all
